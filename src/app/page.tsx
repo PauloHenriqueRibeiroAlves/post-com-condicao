cb02e4db07a034c0971f8216582b85b7112e6b88
Paulo Henrique <paulohenriqueribeiroalves@gmail.com>
2024-03-10 12:54:22 -0300

    descomentando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,51 +4,51 @@ import { Post } from "./types/Post";
 import { useEffect, useState } from "react";
 
 
-// const Page = () => { 
-//   const [possts, setPosts] = useState<Post[]>([]);
-//   const [loading, setLoading] = useState(false);
+const Page = () => { 
+  const [possts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(false);
 
-//   useEffect(() => {
-//     loadPosts();
-//   }, []);
+  useEffect(() => {
+    loadPosts();
+  }, []);
 
 
 
   
-//   const loadPosts = async () => {
-//     let response = await fetch('https://jsonplaceholder.typicode.com/posts');
-//     let json = await response.json();
-//     setPosts(json);
-//   }
+  const loadPosts = async () => {
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let json = await response.json();
+    setPosts(json);
+  }
    
   
-//   return (
-//     <div className="p-5">
+  return (
+    <div className="p-5">
       
-//       {loading &&
-//         <div>Carregando, espere...</div>
-//       }
+      {loading &&
+        <div>Carregando, espere...</div>
+      }
       
-//       {!loading && possts.length > 0 &&
-//         <>
-//           <div>Total de Posts: {possts.length}</div>
-//           <div>
-//             {possts.map((item, index) => (
-//               <div key={index} className="my-4">
-//                 <h4 className="font-bold">{item.title}</h4>
-//                 <small>#{item.id} - Usuário: {item.userId}</small>
-//                 <p>{item.body}</p>
-//               </div>
-//             ))}
-//           </div>
-//         </>
-//       }
-//       {!loading && possts.length === 0 &&
-//         <div>Não ha posts para exibir.</div>
-//       }
+      {!loading && possts.length > 0 &&
+        <>
+          <div>Total de Posts: {possts.length}</div>
+          <div>
+            {possts.map((item, index) => (
+              <div key={index} className="my-4">
+                <h4 className="font-bold">{item.title}</h4>
+                <small>#{item.id} - Usuário: {item.userId}</small>
+                <p>{item.body}</p>
+              </div>
+            ))}
+          </div>
+        </>
+      }
+      {!loading && possts.length === 0 &&
+        <div>Não ha posts para exibir.</div>
+      }
 
       
-//     </div>             
-//   );
-// }
-// export default Page;
+    </div>             
+  );
+}
+export default Page;
